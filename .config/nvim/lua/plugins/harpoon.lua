0ee267ef352f691855ed15d23a12ebc6bdfd104c
MikeDotMpeg <4461292+MikeDotMpeg@users.noreply.github.com>
2024-01-16 08:58:41 -0500

    Update harpoon.lua

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
 		{ "<leader>a", function() require("harpoon"):list():append() end },
-		{ "<M-6>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end },
+		{ "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end },
 		{ "<M-0>", function() require("harpoon"):list():select(1) end },
 		{ "<M-9>", function() require("harpoon"):list():select(2) end },
 		{ "<M-8>", function() require("harpoon"):list():select(3) end },
