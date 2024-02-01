348b69524246e8e1a89a35330e685e70115818ee
MikeDotMpeg <4461292+MikeDotMpeg@users.noreply.github.com>
2024-01-31 22:25:41 -0500

    Update leap.lua

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -18,5 +18,10 @@ return {
     require("leap").opts.highlight_unlabeled_phase_one_targets = true
         vim.api.nvim_set_hl(0, "LeapLabelPrimary", {fg = "yellow", bold = true, nocombine = true })
         vim.api.nvim_set_hl(0, "LeapLabelSecondary", {fg = "blue", bold = true, nocombine = true })
-  end,
+        require('leap').opts.safe_labels = {}
+
+        vim.keymap.set('n', 's', function ()
+                require('leap').leap { target_windows = { vim.api.nvim_get_current_win() } }
+        end)
+end,
 }
