10f1fde3742b9607cd1a47bb4e8a7af15bf26786
MikeDotMpeg <4461292+MikeDotMpeg@users.noreply.github.com>
2024-02-02 17:41:40 -0500

    Update leap.lua

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,27 +1,44 @@
 return {
-  "ggandor/leap.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    require('leap').create_default_mappings()
-    vim.api.nvim_set_hl(0, "LeapBackdrop", {link = "Comment"})
-    vim.api.nvim_set_hl(
-      0,
-      "LeapMatch",
-      {
-        fg = "white", -- for light themes, set to 'black' or similar
-        bold = true,
-        nocombine = true
-      }
-    )
-    require("leap").opts.highlight_unlabeled_phase_one_targets = true
-        vim.api.nvim_set_hl(0, "LeapLabelPrimary", {fg = "yellow", bold = true, nocombine = true })
-        vim.api.nvim_set_hl(0, "LeapLabelSecondary", {fg = "blue", bold = true, nocombine = true })
-        require('leap').opts.safe_labels = {}
-
-        vim.keymap.set('n', 's', function ()
-                require('leap').leap { target_windows = { vim.api.nvim_get_current_win() } }
-        end)
-end,
+    {
+        "ggandor/flit.nvim",
+        keys = function()
+            local ret = {}
+            for _, key in ipairs {"f", "F", "t", "T"} do
+                ret[#ret + 1] = {key, mode = {"n", "x", "o"}, desc = key}
+            end
+            return ret
+        end,
+        opts = {labeled_modes = "nx"},
+        dependencies = {
+            "ggandor/leap.nvim",
+            dependencies = {
+                "tpope/vim-repeat"
+            },
+            config = function()
+                require("leap").create_default_mappings()
+                vim.api.nvim_set_hl(0, "LeapBackdrop", {link = "Comment"})
+                vim.api.nvim_set_hl(
+                    0,
+                    "LeapMatch",
+                    {
+                        fg = "white", -- for light themes, set to 'black' or similar
+                        bold = true,
+                        nocombine = true
+                    }
+                )
+                require("leap").opts.highlight_unlabeled_phase_one_targets = true
+                vim.api.nvim_set_hl(0, "LeapLabelPrimary", {fg = "yellow", bold = true, nocombine = true})
+                vim.api.nvim_set_hl(0, "LeapLabelSecondary", {fg = "gray", bold = true, nocombine = true})
+                --        require('leap').opts.safe_labels = {}
+                vim.keymap.set(
+                    "n",
+                    "s",
+                    function()
+                        require("leap").leap {target_windows = {vim.api.nvim_get_current_win()}}
+                    end
+                )
+            end
+            --
+        }
+    }
 }
