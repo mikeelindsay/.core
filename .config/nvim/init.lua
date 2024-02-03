de39c0a5c5e6551341f1aa8045109aeac3bbb1c5
MikeDotMpeg <4461292+MikeDotMpeg@users.noreply.github.com>
2024-02-03 15:37:10 -0500

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,33 @@
+	-- Define a function to highlight trailing whitespaces and sequences of more than one space between words
+local function highlightExtraSpaces()
+  -- Define the highlight group
+  vim.cmd('highlight ExtraSpaces ctermbg=red guibg=red')
+
+  -- Check if the match IDs exist and are valid before trying to delete them
+  if vim.g.extra_spaces_match_id_trailing and vim.g.extra_spaces_match_id_trailing > 0 then
+    vim.fn.matchdelete(vim.g.extra_spaces_match_id_trailing)
+  end
+  if vim.g.extra_spaces_match_id_indent and vim.g.extra_spaces_match_id_indent > 0 then
+    vim.fn.matchdelete(vim.g.extra_spaces_match_id_indent)
+  end
+
+  -- Add match for trailing whitespaces
+  vim.g.extra_spaces_match_id_trailing = vim.fn.matchadd('ExtraSpaces', '\\s\\+$')
+
+  -- Add match for sequences of more than one space between words
+  -- The pattern now specifically looks for two or more spaces following a non-whitespace character but only if it is not at the start of the line
+  vim.g.extra_spaces_match_id_indent = vim.fn.matchadd('ExtraSpaces', '\\S\\zs\\s\\{2,}\\ze\\S')
+end
+
+-- Set up an autocmd to highlight spaces when entering a buffer or text is changed
+vim.api.nvim_create_autocmd({'BufEnter', 'TextChanged', 'InsertLeave'}, {
+  pattern = '*',
+  callback = highlightExtraSpaces,
+})
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
 vim.opt.colorcolumn = {"80", "130"}
 --vim.opt.scrolloff = 99999
 vim.opt.scrolloff = 5
@@ -10,6 +40,8 @@ vim.api.nvim_command [[ set nofixendofline ]]
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.opt.listchars = 'tab:»»,extends:›,precedes:‹,nbsp:·,trail:·'
+vim.opt.list = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -32,3 +64,4 @@ require("lazy").setup("plugins", {
     border = "single",
   }
 })
+
