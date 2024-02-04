5766d313d08b8bf2d62b564baf2d9c8ccdd4708e
MikeDotMpeg <4461292+MikeDotMpeg@users.noreply.github.com>
2024-02-03 20:56:58 -0500

    Update onedark.lua

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -2,7 +2,13 @@ return {
 	'navarasu/onedark.nvim',
 	config = function()
 		local onedark = require('onedark')
-		onedark.setup({ style = 'darker' })
+		onedark.setup( { style = 'darker',
+			highlights = {
+				["Comment"] = { fg = "#007f00" },
+				["@comment"] = { fg = "#007f00" },
+				["@lsp.type.comment"] = { fg = "#007f00" },
+			}
+		})
 		onedark.load()
 	end,
 	lazy=false,
