cd3e117222cabcf4c0c4ad286eb5558bb8679590
Michael Lindsay <me@me.com>
2025-01-05 21:18:41 -0500

    updating indent blankline

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -10,14 +10,13 @@ return {
 		-- create the highlight groups in the highlight setup hook, so they are reset
 		-- every time the colorscheme changes
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#216631" })
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ffffff" })
 		end)
 
 		require("ibl").setup({
 			indent = {
 				char = "│",
 				tab_char = "│",
-				highlight = highlight
 			}
 		})
 	end
