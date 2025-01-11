b13d5f52f46f6d4f6075837de63a4210075ba277
Michael Lindsay <4461292+mikeelindsay@users.noreply.github.com>
2025-01-10 21:57:38 -0500

    Update lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
-
 return {
 	"williamboman/mason-lspconfig.nvim",
 	dependencies = {
+		"stevearc/conform.nvim",
 		"williamboman/mason.nvim",
 		"neovim/nvim-lspconfig",
 		'hrsh7th/cmp-nvim-lsp',
@@ -13,42 +13,52 @@ return {
 		'zbirenbaum/copilot.lua',
 		'zbirenbaum/copilot-cmp',
 		'github/copilot.vim',
+		-- Do i need fidget?
+		"j-hui/fidget.nvim",
 		'Hoffs/omnisharp-extended-lsp.nvim',
 	},
 	config = function()
 		local mason = require("mason")
-
+		require('fidget').setup({})
 		mason.setup()
-
-		 require("mason-lspconfig").setup {
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+		})
+		require("mason-lspconfig").setup {
 			ensure_installed = {
 				"lua_ls",
 				"powershell_es",
 				"omnisharp"
 			}
 		}
-local rounded_border_handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-}
-	vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+
+		local rounded_border_handlers = {
+			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+		}
 
 		local lspconfig = require("lspconfig")
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-		lspconfig.lua_ls.setup(
-		    {
+		lspconfig.lua_ls.setup({
 			capabilities = lsp_capabilities,
 			settings = {
-			    Lua = {
-				diagnostics = {
-				    globals = {"vim"}
+				Lua = {
+					diagnostics = {
+						globals = {"vim"}
+					}
 				}
-			    }
 			}
-		    }
+		}
 		)
 
 		lspconfig.powershell_es.setup(
@@ -56,7 +66,7 @@ local rounded_border_handlers = {
 				capabilities = lsp_capabilities
 			}
 		)
-lspconfig.omnisharp.setup({
+		lspconfig.omnisharp.setup({
 			on_attach = on_attach,
 			capabilities = capabilities,
 			root_dir = function(fname)
@@ -73,56 +83,50 @@ lspconfig.omnisharp.setup({
 			}),
 		})
 		local cmp = require "cmp"
-		local has_words_before = function()
-			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-				return false
-			end
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-		end
+
 		cmp.setup(
-		    {
-			experimental = {ghost_text = false},
-			snippet = {
-				expand = function(args)
-				vim.fn["vsnip#anonymous"](args.body)
-			    end
-			},
-			window = {
-			    completion = cmp.config.window.bordered(),
-			    documentation = cmp.config.window.bordered()
-			},
-			mapping = cmp.mapping.preset.insert(
-			    {
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({select = true})
-			    }
-			),
-			sources = cmp.config.sources(
-			    {
-				{name = "nvim_lsp"},
-				{name = "vsnip"}
-			    },
-			    {
-				{name = "buffer"}
-			    }
-			)
-		    }
+			{
+				experimental = {ghost_text = false},
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered()
+				},
+				mapping = cmp.mapping.preset.insert(
+					{
+						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm({select = true})
+					}
+				),
+				sources = cmp.config.sources(
+					{
+						{name = "nvim_lsp"},
+						{name = "vsnip"}
+					},
+					{
+						{name = "buffer"}
+					}
+				)
+			}
 		)
 		cmp.setup.cmdline(
-		    {
-			    "/",
-			    "?"
-		    },
-		    {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-			    {name = "buffer"}
+			{
+				"/",
+				"?"
+			},
+			{
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{name = "buffer"}
+				}
 			}
-		    }
 		)
 
 		cmp.setup.cmdline(
