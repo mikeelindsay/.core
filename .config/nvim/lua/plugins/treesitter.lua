5e091375babf442d5923df322b6721c728157ab5
Michael Lindsay <4461292+mikeelindsay@users.noreply.github.com>
2025-01-10 22:02:35 -0500

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,43 +1,44 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "nvim-treesitter/playground",
-    },
-    build = function()
-        vim.cmd([[TSUpdate]])
-    end,
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "c",
-                "c_sharp",
-                "dockerfile",
-                "git_config",
-                "git_rebase",
-                "go",
-                "gomod",
-                "gosum",
-                "gowork",
-                "javascript",
-                "json",
-                "json5",
-                "jsonc",
-                "lua",
-                "python",
-                "sql",
-                "vimdoc",
-                "yaml",
-                "zig",
-            },
-            sync_install = false,
-            auto_install = false,
-            ignore_install = {},
-            modules = {},
-            highlight = {
-                enable = true,
-                disable = { "dockerfile" },
-            },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	lazy=false,
+	dependencies = {
+		"nvim-treesitter/playground",
+	},
+	build = function()
+		vim.cmd([[TSUpdate]])
+	end,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"c_sharp",
+				"dockerfile",
+				"git_config",
+				"git_rebase",
+				"go",
+				"gomod",
+				"gosum",
+				"gowork",
+				"javascript",
+				"json",
+				"json5",
+				"jsonc",
+				"lua",
+				"python",
+				"sql",
+				"vimdoc",
+				"yaml",
+			},
+			sync_install = false,
+			auto_install = false,
+			ignore_install = {},
+			modules = {},
+			highlight = {
+				enable = true,
+				disable = { "dockerfile" },
+				additional_vim_regex_highlighting = false
+			},
+			indent = { enable = true },
+		})
+	end,
 }
